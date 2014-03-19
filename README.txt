8fc5c9c455ca12c0af4c35dd3a40f923f08c98c8
Dmitry Baryshev <dmitrymq@gmail.com>
2014-03-19 18:05:23 +0000

    Initial commit

---
+++ README.txt
@@ -0,0 +1,17 @@
+QssEditor
+---------
+
+Author: Dmitry Baryshev <linuxsquirrel.dev@gmail.com>
+License: GNU GPLv3+ (see LICENSE.txt),
+         QScintilla parts are under GNU GPL v3,
+         all UI files are licensed in the same manner as the main project
+
+Other
+-----
+
+* Other icons are taken from Fugue Icons theme
+
+http://p.yusukekamiyamane.com/
+
+Author: Yusuke Kamiyamane
+License: Creative Commons Attribution 3.0 License
---
+++ qsseditor.h
@@ -0,0 +1,66 @@
+/*
+ * This file is part of QssEditor.
+ *
+ * QssEditor is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * QssEditor is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with QssEditor. If not, see <http://www.gnu.org/licenses/>.
+ */
+
+#ifndef QSSEDITOR_H
+#define QSSEDITOR_H
+
+#include <QWidget>
+
+class QTimer;
+
+namespace Ui
+{
+    class QssEditor;
+}
+
+class QssEditor : public QWidget
+{
+    Q_OBJECT
+
+public:
+    explicit QssEditor(QWidget *parent = 0);
+    ~QssEditor();
+
+protected:
+    virtual bool eventFilter(QObject *obj, QEvent *event);
+    virtual void closeEvent(QCloseEvent *);
+
+private:
+    void open(const QString &fileName);
+    bool save();
+    QString settingsErrorToString(int);
+    bool continueWhenUnsaved();
+
+private slots:
+    void slotCssChanged();
+    void slotApplyCss();
+    void slotOpen();
+    void slotSave();
+    void slotSaveAs();
+    void slotOptions();
+    void slotProgress();
+    void slotQuit();
+    void slotDelayedOpen();
+
+private:
+    Ui::QssEditor *ui;
+    QTimer *m_timerDelayedApply;
+    bool m_changed;
+    QString m_lastFileName;
+};
+
+#endif // QSSEDITOR_H
