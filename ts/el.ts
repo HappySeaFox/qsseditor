0629878b2f6799312af09c5b3b0bcbabdc1672cb
Dmitry Baryshev <dmitrymq@gmail.com>
2021-03-02 16:34:57 +0300

    Added lost el.ts

---
+++ ts/el.ts
@@ -0,0 +1,285 @@
+<?xml version="1.0" encoding="utf-8"?>
+<!DOCTYPE TS>
+<TS version="2.1" language="el">
+<context>
+    <name>About</name>
+    <message>
+        <location filename="../about.ui" line="68"/>
+        <source>OK</source>
+        <translation>OK</translation>
+    </message>
+    <message>
+        <location filename="../about.ui" line="103"/>
+        <source>QSS Editor is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.</source>
+        <extracomment>See https://www.gnu.org/licenses/gpl-2.0.txt . &quot;THT&quot; is the name of the application</extracomment>
+        <translation>QSS Εκδότης διανέμεται με την ελπίδα ότι θα είναι χρήσιμος, αλλά ΧΩΡΙΣ ΚΑΜΙΑ ΕΓΓΥΗΣΗ? χωρίς καν την υπονοούμενη εγγύηση ΕΜΠΟΡΕΥΣΙΜΟΤΗΤΑΣ ή ΚΑΤΑΛΛΗΛΟΤΗΤΑΣ ΓΙΑ ΕΝΑ ΣΥΓΚΕΚΡΙΜΕΝΟ ΣΚΟΠΟ.</translation>
+    </message>
+    <message>
+        <location filename="../about.ui" line="122"/>
+        <source>QSS Editor is a tool to edit and preview Qt style sheets</source>
+        <extracomment>&quot;THT&quot; is the name of the application. The URL to visit will be added in runtime. Ticker is a short company name, see http://www.investopedia.com/terms/t/tickersymbol.asp . The common practice is to borrow &quot;ticker&quot; from English and incorporate into your language (see http://en.wikipedia.org/wiki/Loanword)</extracomment>
+        <translation>Το QSS Editor είναι ένα εργαλείο για να επεξεργαστείτε και να κάνετε προεπισκόπηση Qt φύλλα στυλ</translation>
+    </message>
+    <message>
+        <location filename="../about.cpp" line="37"/>
+        <source>Dmitry Baryshev</source>
+        <translation>Dmitry Baryshev</translation>
+    </message>
+</context>
+<context>
+    <name>Options</name>
+    <message>
+        <location filename="../options.ui" line="14"/>
+        <source>Options</source>
+        <translation>Επιλογές</translation>
+    </message>
+    <message>
+        <location filename="../options.ui" line="22"/>
+        <source>Language:</source>
+        <translation>Γλώσσα:</translation>
+    </message>
+    <message>
+        <location filename="../options.ui" line="57"/>
+        <source>Open last style at startup</source>
+        <translation>Ανοιχτό τελευταίο στυλ κατά την εκκίνηση</translation>
+    </message>
+    <message>
+        <location filename="../options.ui" line="66"/>
+        <source>Preview delay:</source>
+        <translation>Καθυστέρηση προεπισκόπησης:</translation>
+    </message>
+    <message>
+        <location filename="../options.ui" line="73"/>
+        <source>ms</source>
+        <translation>μδευ</translation>
+    </message>
+    <message>
+        <location filename="../options.ui" line="120"/>
+        <source>Need restart</source>
+        <translation>Απαιτείται επανεκκίνηση </translation>
+    </message>
+    <message>
+        <location filename="../options.cpp" line="35"/>
+        <source>System</source>
+        <translation>Σύστημα</translation>
+    </message>
+</context>
+<context>
+    <name>QObject</name>
+    <message>
+        <location filename="../project.cpp" line="38"/>
+        <source>File name is empty</source>
+        <translation>Όνομα αρχείου είναι άδειο</translation>
+    </message>
+    <message>
+        <location filename="../project.cpp" line="130"/>
+        <source>The data stream has read past the end of the data</source>
+        <translation>Το ρεύμα δεδομένων έλαβε γνώση μετά το τέλος των δεδομένων</translation>
+    </message>
+    <message>
+        <location filename="../project.cpp" line="133"/>
+        <source>The data stream has read corrupt data</source>
+        <translation>Η ροή δεδομένων έλαβε γνώση διεφθαρμένων στοιχείων</translation>
+    </message>
+    <message>
+        <location filename="../project.cpp" line="137"/>
+        <source>The data stream cannot write to the underlying device</source>
+        <translation>Η ροή δεδομένων δεν μπορεί να γράψει με την υποκείμενη συσκευή</translation>
+    </message>
+    <message>
+        <location filename="../project.cpp" line="140"/>
+        <source>Unknown error</source>
+        <translation>Άγνωστο σφάλμα</translation>
+    </message>
+    <message>
+        <location filename="../tools.h" line="39"/>
+        <source>About QSS Editor</source>
+        <translation>Σχετικά με το QSS Editor</translation>
+    </message>
+</context>
+<context>
+    <name>QssEditor</name>
+    <message>
+        <location filename="../qsseditor.ui" line="29"/>
+        <location filename="../qsseditor.cpp" line="410"/>
+        <source>Open style</source>
+        <translation>Άνοιγμα στυλ</translation>
+    </message>
+    <message>
+        <location filename="../qsseditor.ui" line="39"/>
+        <source>Save style</source>
+        <translation>Αποθήκευση στυλ</translation>
+    </message>
+    <message>
+        <location filename="../qsseditor.ui" line="46"/>
+        <location filename="../qsseditor.cpp" line="432"/>
+        <source>Save style as</source>
+        <translation>Αποθήκευση στυλ ως</translation>
+    </message>
+    <message>
+        <location filename="../qsseditor.ui" line="72"/>
+        <source>Close</source>
+        <translation>Κλείσιμο</translation>
+    </message>
+    <message>
+        <location filename="../qsseditor.ui" line="92"/>
+        <source>Undo</source>
+        <translation>Αναίρεση</translation>
+    </message>
+    <message>
+        <location filename="../qsseditor.ui" line="99"/>
+        <source>Redo</source>
+        <translation>Επανάληψη</translation>
+    </message>
+    <message>
+        <location filename="../qsseditor.ui" line="119"/>
+        <source>Options</source>
+        <translation>Επιλογές</translation>
+    </message>
+    <message>
+        <location filename="../qsseditor.ui" line="172"/>
+        <source>Preview style</source>
+        <translation>Στυλ προεπισκόπησης</translation>
+    </message>
+    <message>
+        <location filename="../qsseditor.cpp" line="205"/>
+        <source>Cannot open style:</source>
+        <translation>Αδύνατο το άνοιγμα του στυλ:</translation>
+    </message>
+    <message>
+        <location filename="../qsseditor.cpp" line="233"/>
+        <source>Cannot save style:</source>
+        <translation>Δεν είναι δυνατή η αποθήκευση του στυλ:</translation>
+    </message>
+    <message>
+        <location filename="../qsseditor.cpp" line="252"/>
+        <source>No error</source>
+        <translation>Κανένα σφάλμα</translation>
+    </message>
+    <message>
+        <location filename="../qsseditor.cpp" line="255"/>
+        <source>File access error</source>
+        <translation>Σφάλμα πρόσβασης Αρχείου</translation>
+    </message>
+    <message>
+        <location filename="../qsseditor.cpp" line="258"/>
+        <source>Malformed file</source>
+        <translation>Ακατάλληλο αρχείο</translation>
+    </message>
+    <message>
+        <location filename="../qsseditor.cpp" line="261"/>
+        <source>Unknown error</source>
+        <translation>Άγνωστο σφάλμα</translation>
+    </message>
+    <message>
+        <location filename="../qsseditor.cpp" line="268"/>
+        <source>Warning</source>
+        <translation>Προειδοποίηση</translation>
+    </message>
+    <message>
+        <location filename="../qsseditor.cpp" line="269"/>
+        <source>This will discard all the unsaved changes</source>
+        <translation>Αυτό θα απορρίψει όλες τις μη αποθηκευμένες αλλαγές</translation>
+    </message>
+    <message>
+        <location filename="../qsseditor.cpp" line="282"/>
+        <source>Cannot open style. The file doesn&apos;t exist or not readable</source>
+        <translation>Δεν είναι δυνατό το άνοιγμα στυλ. Το αρχείο δεν υπάρχει ή δεν είναι αναγνώσιμο</translation>
+    </message>
+    <message>
+        <location filename="../qsseditor.cpp" line="288"/>
+        <source>Cannot change directory</source>
+        <translation>Δεν είναι δυνατή η αλλαγή καταλόγου</translation>
+    </message>
+    <message>
+        <location filename="../qsseditor.cpp" line="302"/>
+        <source>Error</source>
+        <translation>Σφάλμα</translation>
+    </message>
+    <message>
+        <location filename="../qsseditor.cpp" line="378"/>
+        <source>QSS Editor</source>
+        <translation>QSS Editor</translation>
+    </message>
+    <message>
+        <location filename="../qsseditor.cpp" line="410"/>
+        <location filename="../qsseditor.cpp" line="432"/>
+        <source>Qt Style Sheets (*.qss)</source>
+        <translation>Qt Style Sheets (*.qss)</translation>
+    </message>
+</context>
+<context>
+    <name>SearchAndReplace</name>
+    <message>
+        <location filename="../searchandreplace.ui" line="14"/>
+        <source>Find and Replace</source>
+        <translation>Εύρεση και αντικατάσταση</translation>
+    </message>
+    <message>
+        <location filename="../searchandreplace.ui" line="20"/>
+        <source>Find:</source>
+        <translation>Εύρεση:</translation>
+    </message>
+    <message>
+        <location filename="../searchandreplace.ui" line="27"/>
+        <source>Replace with:</source>
+        <translation>Αντικατάσταση με:</translation>
+    </message>
+    <message>
+        <location filename="../searchandreplace.ui" line="34"/>
+        <source>Options</source>
+        <translation>Επιλογές</translation>
+    </message>
+    <message>
+        <location filename="../searchandreplace.ui" line="40"/>
+        <source>Case sensitive</source>
+        <translation>Υπόθεση ευαίσθητη</translation>
+    </message>
+    <message>
+        <location filename="../searchandreplace.ui" line="47"/>
+        <source>Whole words</source>
+        <translation>Ολόκληρες λέξεις</translation>
+    </message>
+    <message>
+        <location filename="../searchandreplace.ui" line="54"/>
+        <source>Regular expression</source>
+        <translation>Κανονική έκφραση</translation>
+    </message>
+    <message>
+        <location filename="../searchandreplace.ui" line="66"/>
+        <source>Find</source>
+        <translation>Εύρεση</translation>
+    </message>
+    <message>
+        <location filename="../searchandreplace.ui" line="76"/>
+        <source>Replace&amp;&amp;Next</source>
+        <extracomment>The sign &quot;&amp;&amp;&quot; means &quot;&amp;&quot; (&quot;AND&quot;). So the button name means &quot;Replace the found text and search again&quot;</extracomment>
+        <translation>Αντικατάσταση &amp; Επόμενο</translation>
+    </message>
+    <message>
+        <location filename="../searchandreplace.ui" line="86"/>
+        <source>Replace All</source>
+        <translation>Αντικατάσταση όλων</translation>
+    </message>
+    <message>
+        <location filename="../searchandreplace.ui" line="93"/>
+        <source>Close</source>
+        <translation>Κλείσιμο</translation>
+    </message>
+    <message>
+        <location filename="../searchandreplace.ui" line="117"/>
+        <source>Direction</source>
+        <translation>Κατεύθυνση</translation>
+    </message>
+    <message>
+        <location filename="../searchandreplace.ui" line="123"/>
+        <source>Up</source>
+        <translation>Επάνω</translation>
+    </message>
+    <message>
+        <location filename="../searchandreplace.ui" line="130"/>
+        <source>Down</source>
+        <translation>Κάτω</translation>
+    </message>
+</context>
+</TS>
